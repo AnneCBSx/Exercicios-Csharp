5833eb2df75be3ecd3458f144fdea46df841428f
Anne Caroline <annianinha@hotmail.com>
2022-09-07 01:27:44 -0300

    inserção de buttons

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,20 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnCacelar = new System.Windows.Forms.Button();
+            this.btnOK = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // btnCacelar
+            // 
+            this.btnCacelar.BackColor = System.Drawing.SystemColors.ControlText;
+            this.btnCacelar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCacelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCacelar.ForeColor = System.Drawing.Color.Orange;
+            this.btnCacelar.Location = new System.Drawing.Point(437, 248);
+            this.btnCacelar.Name = "btnCacelar";
+            this.btnCacelar.Size = new System.Drawing.Size(98, 31);
+            this.btnCacelar.TabIndex = 0;
+            this.btnCacelar.Text = "Cancelar";
+            this.btnCacelar.UseVisualStyleBackColor = false;
+            this.btnCacelar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnOK
+            // 
+            this.btnOK.BackColor = System.Drawing.SystemColors.ControlText;
+            this.btnOK.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnOK.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOK.ForeColor = System.Drawing.Color.Orange;
+            this.btnOK.Location = new System.Drawing.Point(346, 248);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(69, 31);
+            this.btnOK.TabIndex = 1;
+            this.btnOK.Text = "Ok";
+            this.btnOK.UseVisualStyleBackColor = false;
+            this.btnOK.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(897, 523);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(547, 303);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.btnCacelar);
+            this.ForeColor = System.Drawing.Color.Red;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Meu sistema";
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnCacelar;
+        private System.Windows.Forms.Button btnOK;
     }
 }
 
