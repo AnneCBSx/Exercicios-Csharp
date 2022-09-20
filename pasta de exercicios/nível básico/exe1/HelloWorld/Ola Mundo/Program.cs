30c9c9029d32d96007fe595fb94686c2b3c0fd0c
Anne Caroline <annianinha@hotmail.com>
2022-09-20 06:40:53 -0300

    primeiro commit

---
+++ pasta de exercicios/nível básico/exe1/HelloWorld/Ola Mundo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ola_Mundo
+    {
+    internal class Program
+        {
+        static void Main(string[] args)
+            {
+            Console.WriteLine ("Hello World!");
+            }
+        }
+    }
